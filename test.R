fe1b06e01b3254759421dd498fb654c8913633ea
John <144583586+Questiomaconmark@users.noreply.github.com>
2025-07-31 01:20:04 -0600

    changes were made

--- test.R
+++ test.R
@@ -3,18 +3,15 @@ library(exploreARTIS)
 
 csv <- read.csv("~/bailey_stuff/snodgras/artis_request_snodgrass.csv")
 
-colnames(csv)
+home_country_consumption <- csv %>%
+  filter(source_country_iso3c == importer_iso3c)
 
-plot <- plot_bar(
-  data = csv,
-  bar_group = "exporter_iso3c",
-  value = "live_weight_t",
-  top_n = 10,
-  x.lab = "Export weight (t)",
-  y.lab = "Country (ISO3)",
-  plot.title = "Top exporting countries",
+home_country_consumption_catfish <- home_country_consumption %>%
+  filter("ictalurus punctatus" == sciname)
+
+test <- plot_ts(home_country_consumption_catfish,
+  artis_var = "importer_iso3c",
+  plot.type = "stacked"
 )
-plot <- plot + ggplot2::ggtitle("Top exporting countries")
 
-
-print(plot)
+print(test)
