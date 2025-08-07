40f829cdbccf8b836119a769bdd48e40ae17d697
John <144583586+Questiomaconmark@users.noreply.github.com>
2025-08-07 01:03:45 -0600

    worked on lionfish_graph

--- lionfish_testing/lionfish_graph.R
+++ lionfish_testing/lionfish_graph.R
@@ -1,10 +1,15 @@
-library(dplyr)
+library(tidyverse)
 library(exploreARTIS)
 
 # CSV created in parquet_testing
-lionfish_data <- read.csv("bailey_stuff/snodgras_capstone_scripts/lionfish_testing/lionfish.csv")
+lionfish_data <- read.csv("bailey_stuff/snodgras_capstone_scripts/lionfish_testing/lionfish_trade.csv")
 
-# ExploreArtis package not updated for new data labels so this is correcting the new data with old labels
-#
+lionfish_data_filtered <- lionfish_data %>%
+  filter(source_country_iso3c == "USA")
 
-plot_ts(lionfish_data, artis_var = "consumption_live_t")
+lionfish_graph <- plot_ts(lionfish_data_filtered, artis_var = "importer_iso3c")
+
+lionfish_graph <- lionfish_graph +
+  geom_vline(xintercept = 2016.99, linetype= "dotted", color ="red")
+
+print(lionfish_graph)
